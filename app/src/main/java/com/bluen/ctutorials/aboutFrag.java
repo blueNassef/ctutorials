d733572a8370728f09df3536353ba49aac1b4552
root <trois4ever@gmail.com>
2017-04-15 05:53:41 +0200

    hello_done

---
+++ app/src/main/java/com/bluen/ctutorials/aboutFrag.java
@@ -0,0 +1,21 @@
+package com.bluen.ctutorials;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by root on 4/14/17.
+ */
+
+public class aboutFrag extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View v= inflater.inflate(R.layout.layout_frag,container,false);
+        return v;
+    }
+}
